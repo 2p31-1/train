445f179ca154064f3930663c2e17c5c41ae48944
hyeonsoft <jbhyun2005@naver.com>
2017-10-05 19:30:11 +0900

    Create control_test.go

---
+++ control_test.go
@@ -0,0 +1,37 @@
+package main
+import (
+	. "github.com/cyoung/rpi"
+	"fmt"
+	"time"
+)
+const pwm = PIN_GPIO_0
+const rev = PIN_GPIO_3
+const put = PIN_GPIO_2
+var a, b int
+func main(){
+	WiringPiSetup()
+	PinMode(pwm, OUTPUT)
+	PinMode(put, OUTPUT)
+	PinMode(rev, OUTPUT)
+	DigitalWrite(put, 1)
+	fmt.Printf("Started")
+	go run()
+	for a>=0{
+		fmt.Scanf("%d %d",&a,&b)
+		fmt.Printf("ok\n")
+	}
+	PinMode(pwm,1)
+	PinMode(put,1)
+	DigitalWrite(pwm,1)
+	DigitalWrite(put,1)
+	Delay(10000)
+}
+
+func run(){
+		for true{
+		DigitalWrite(pwm,1)
+		time.Sleep(time.Duration(a) * time.Nanosecond)
+		DigitalWrite(pwm,0)
+		time.Sleep(time.Duration(b) * time.Nanosecond)
+	}
+}
